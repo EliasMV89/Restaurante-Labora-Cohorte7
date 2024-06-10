b0591f30f8347b4a59a2233fb0d6096c9e75d7e7
EliasMVillagra <eliasmvillagra@gmail.com>
2024-06-10 14:26:51 -0300

    modifique varios archivos

--- services/cliente.go
+++ services/cliente.go
@@ -1,33 +1,3 @@
-/*
-package services
-
-import (
-
-	"database/sql"
-	"fmt"
-	"log"
-
-)
-
-	type Cliente struct {
-		Nombre   string
-		Telefono string
-		Email    string
-	}
-
-	func AgregarCliente(db *sql.DB, cliente Cliente) error {
-		// Consulta para agregar un cliente
-		query := `INSERT INTO Clientes (nombre, telefono, email) VALUES (?,?,?)`
-		// Ejecuta la consulta
-		_, err := db.Exec(query, cliente.Nombre, cliente.Telefono, cliente.Email)
-		if err != nil {
-			log.Printf("Error al insertar cliente: %v", err)
-			return err
-		}
-		fmt.Println("Cliente agregado correctamente.")
-		return nil
-	}
-*/
 package services
 
 import (
